18951ef8f12af5023fae80407ba5887b2a32b752
kaustav03 <kaustavghosh444@gmail.com>
2025-05-18 13:05:37 +0530

    blog

---
+++ blog.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog Banner</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles/blog.css">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400..700;1,400..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <section id="header">
+        <?php include 'header.php'; ?>
+    </section>
+    <main>
+        <div>
+            <section id="blog-banner">
+                <div class="global-banner global-banner-others others-banner-mobile">
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-5 d-flex align-items-center">
+                                <div class="blog-banner-text">
+                                    <h1>Blog</h1>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut
+                                        labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="blog-banner-image">
+                                    <img src="./assets/images/Blog Bannert.png" alt="blogs">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div class='body-curve-left home-aboutus'>
+                <section class="gap-top gap-bottom" id="blog-content">
+                    <div class="container-xxl blog-content-container">
+                        <div class="row">
+                            <div class="d-flex gap-5 align-items-center blog-content flex-wrap">
+                                <div class="col-sm-5 blog-content-image">
+                                    <img src="./assets/images/Blog Cont 1.png" alt="">
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="blog-content-text">
+                                        <h5 class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet</h5>
+                                        <p class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet, consectetur
+                                            adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor
+                                            sit amet,
+                                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                            dolore magna
+                                            aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container-xxl">
+                        <div class="row gap-top">
+                            <div
+                                class="d-flex gap-5 align-items-center blog-content flex-wrap flex-column-reverse flex-sm-row">
+                                <div class="col-sm-6">
+                                    <div class="blog-content-text">
+                                        <h5 class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet</h5>
+                                        <p class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet, consectetur
+                                            adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor
+                                            sit amet,
+                                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                            dolore magna
+                                            aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                                <div class="col-sm-5 blog-content-image">
+                                    <img src="./assets/images/Blog Cont 2.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container-xxl">
+                        <div class="row gap-top">
+                            <div class="d-flex gap-5 align-items-center blog-content flex-wrap">
+                                <div class="col-sm-5 blog-content-image">
+                                    <img src="./assets/images/Blog Cont 3.png" alt="">
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="blog-content-text">
+                                        <h5 class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet</h5>
+                                        <p class="pb-2 pb-sm-2 pb-lg-4">Lorem ipsum dolor sit amet, consectetur
+                                            adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor
+                                            sit amet,
+                                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                            dolore magna
+                                            aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="gap-top gap-bottom" id="blog-like">
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <div class="blog-like-text">
+                                    <h6>You may like</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="d-flex flex-wrap justify-content-center gap-4">
+                                    <div class="blog-like-card">
+                                        <div class="blog-like-card-image">
+                                            <img src="./assets/images/Blog Like 1.png" alt="Blog Cover">
+                                        </div>
+                                        <div class="blog-like-content">
+                                            <h5>Lorem ipsum dolor sit amet</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                                tempor
+                                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                                                consectetur
+                                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                                magna aliqua.
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="blog-like-card">
+                                        <div class="blog-like-card-image">
+                                            <img src="./assets/images/Blog Like 2.png" alt="Blog Cover">
+                                        </div>
+                                        <div class="blog-like-content">
+                                            <h5>Lorem ipsum dolor sit amet</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                                tempor
+                                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                                                consectetur
+                                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                                magna aliqua.
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="blog-like-card">
+                                        <div class="blog-like-card-image">
+                                            <img src="./assets/images/Blog Like 3.png" alt="Blog Cover">
+                                        </div>
+                                        <div class="blog-like-content">
+                                            <h5>Lorem ipsum dolor sit amet</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                                tempor
+                                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                                                consectetur
+                                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                                magna aliqua.
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class='body-curve-right home-footer'>
+                <section class="gap-top gap-bottom" id="feedback">
+                    <?php include './templates/feedback.php'; ?>
+                </section>
+                <section class="gap-top gap-bottom" id="contact-section">
+
+                </section>
+            </div>
+        </div>
+    </main>
+    <section class="gap-top" id="footer">
+        <?php include 'footer.php'; ?>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
