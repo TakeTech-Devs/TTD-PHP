ced83233867889043ecf2f39495a1dc62045109a
kaustav03 <kaustavghosh444@gmail.com>
2025-05-18 13:48:00 +0530

    services

---
+++ services.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>service Banner</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles/services.css">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400..700;1,400..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <section id="header">
+        <?php include 'header.php'; ?>
+    </section>
+    <main>
+        <div>
+            <section id="service-banner">
+                <div class="global-banner global-banner-others others-banner-mobile">
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-5 d-flex align-items-center">
+                                <div class="service-banner-text">
+                                    <h1>Our Services</h1>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut
+                                        labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="service-banner-image">
+                                    <img src="./assets/images/servicebanner.png" alt="Services">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div class='body-curve-left home-aboutus'>
+                <section class="gap-top gap-bottom" id="service-content">
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <div class="service-content-text">
+                                    <h2>Services</h2>
+                                    <h5>Services We Offer:</h5>
+                                    <p>Take a tour of the services we provide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row service-content-header">
+                            <div class="d-flex gap-2 align-items-center service-content-main gap-top">
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/Web Solutions.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>Web Technology</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                class="d-flex gap-2 flex-column-reverse flex-sm-row align-items-center service-content-main">
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>Software Solutions</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/Software Solutions.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 align-items-center service-content-main">
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/Graphics Solutions.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>Graphical Solutions</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                class="d-flex gap-2 align-items-center service-content-main flex-column-reverse flex-sm-row">
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>Mobile App Solutions</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore
+                                            et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/Mobile App Solutions.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 align-items-center service-content-main">
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/Marketing Solutions.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>Marketing Solutions</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore
+                                            et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                class="d-flex gap-2 align-items-center service-content-main flex-column-reverse flex-sm-row">
+                                <div class="col-sm-6">
+                                    <div class="service-content-section">
+                                        <h5>AI Solutions</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                            tempor incididunt ut
+                                            labore
+                                            et dolore magna aliqua.</p>
+                                        <button class="secondary-button">Read More</button>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="service-content-image">
+                                        <img src="./assets/images/AI Solutions.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class='body-curve-right home-footer'>
+                <section class="gap-top gap-bottom" id="service-providing">
+                    <div class="container-xxl my-5">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="feedback-text text-start">
+                                    <h2>What Can TakeTech Devs Provide You?</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="container-xxl">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-md-5">
+                                <p>
+                                    TakeTech Devs is one of the top web development companies in India. We believe your
+                                    ideas are unique,
+                                    and we simply help to transform your ideas into reality. TakeTech has provided many
+                                    Website Design and
+                                    Website Development services with modernized features and techniques.
+                                </p>
+                                <p>
+                                    We provide 100% responsive website compatible with desktops, laptops, tablets, and
+                                    mobile phones.
+                                    Along with this, businesses will also get end-to-end support from us, free of cost.
+                                </p>
+                            </div>
+
+                            <div class="col-md-7">
+                                <div class="row service-right-grid">
+                                    <div class="col-sm-6 d-flex justify-content-center">
+                                        <div
+                                            class="service-right-content d-flex flex-column align-items-center text-center">
+                                            <div class="service-right-content-image mb-3">
+                                                <img src="./assets/images/Service Icon 4.png" alt="">
+                                            </div>
+                                            <p>100% Responsive Website</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 d-flex justify-content-center">
+                                        <div
+                                            class="service-right-content d-flex flex-column align-items-center text-center">
+                                            <div class="service-right-content-image mb-3">
+                                                <img src="./assets/images/Service Icon 2.png" alt="">
+                                            </div>
+                                            <p>Support after project completion</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 d-flex justify-content-center">
+                                        <div
+                                            class="service-right-content d-flex flex-column align-items-center text-center">
+                                            <div class="service-right-content-image mb-3">
+                                                <img src="./assets/images/Service Icon 1.png" alt="">
+                                            </div>
+                                            <p>Full Premium Quality Product</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 d-flex justify-content-center">
+                                        <div
+                                            class="service-right-content d-flex flex-column align-items-center text-center">
+                                            <div class="service-right-content-image mb-3">
+                                                <img src="./assets/images/Service Icon 3.png" alt="">
+                                            </div>
+                                            <p>Free Consultation for projects/ any queries</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="gap-top gap-bottom" id="service-choose">
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="service-choose-text">
+                                    <h2>Why Choose TakeTech Devs?</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container-xxl">
+                        <div class="row">
+                            <div class="col-sm-6 d-flex align-items-center">
+                                <div class="service-choose-content">
+                                    <p>TakeTech Devs, the right people in the right place.</p>
+                                    <p>TakeTech Devs has provided many Website designs & Website Development services
+                                        with Modern
+                                        features And Techniques. We, in TakeTech Devs, pay attention to the client's
+                                        needs and our
+                                        experienced team members always deliver the best to you, so that you can take
+                                        your business to
+                                        another level. For each project we take, we constantly have a communication with
+                                        our clients
+                                        which helps us to deliver the best/full premium quality product. As we follow an
+                                        agile workflow,
+                                        it helps us work smoothly and efficiently. We are dedicated to the client's/
+                                        your success and
+                                        give priority to your satisfaction. So, any Business or Start-Ups can rely on us
+                                        for any website
+                                        development projects.</p>
+                                    <p>Just feel free to contact us, we make your tech and IT dreams a reality.</p>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="service-choose-image">
+                                    <img src="./assets/images/Service Choose.png" alt="Desktop" class="desktop">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="gap-top gap-bottom" id="feedback">
+                    <?php include './templates/feedback.php'; ?>
+                </section>
+                <section class="gap-top gap-bottom" id="contact-section">
+
+                </section>
+            </div>
+        </div>
+    </main>
+    <section class="gap-top" id="footer">
+        <?php include 'footer.php'; ?>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
